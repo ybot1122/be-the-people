6a011b9382f9b4e73443bcf1bc2468b5b5315e40
Nakul Malhotra <nakulm95@uw.edu>
2014-09-21 15:58:29 -0700

    content.js changed

--- js/content.js
+++ js/content.js
@@ -8,7 +8,7 @@
 	and then uses Mustache templating to render the contents. manages the sliding
 	transition animation also.
 		pagename 	=> 	string identifying the page to load content for
-		yoolololololol
+		so i changed this file with this new change.
 */
 function updateContent(pagename) {
 	var $button = $('#' + pagename);
