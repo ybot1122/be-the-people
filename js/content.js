e4a4ed8f478e254814776c053456d9441df1c58a
JFrazier56 <jason.frazier56@yahoo.com>
2014-11-13 20:33:24 -0800

    Added the sliding animations for the infocolumns. The home button is the logo for be the people in the corner, to make them animate back to the original page.

--- js/content.js
+++ js/content.js
@@ -9,6 +9,27 @@
 */
 function changeActivePage(pagename) {
 	var $button = $('#' + pagename);
+
+	//If the home button is clicked, animate all the columns back to the original width and margin
+	if($button.is('#home')){
+		$('.infocolumn').animate({
+			width: '30%',
+			marginLeft:'30'
+		}, 600)
+	} else { 
+		//make the active column 95%, while making the others 0% width and remove their margin for sizing issues
+		$button.animate({width:'95%'}, 600);
+		$('.infocolumn').not($button).animate({
+			width:'0%', 
+			marginLeft:'0'
+		}, 600);
+	}
+
+	// TODO: make request to server, get the data, and render the template
+	loadContent(pagename, function(data) {
+		loadTemplate($('#content div'), '#template-' + pagename, 'general.html', data, function(){})
+	});
+	/*
 	// active page; clicking again means you wanna close it
 	if ($button.is('span[data-active=\"true\"]')) {
 		// content pane already open. just close it.
@@ -22,15 +43,11 @@ function changeActivePage(pagename) {
 			$('#loading').show();
 			// TODO: make request to server, get the data, and render the template
 			loadContent(pagename, function(data) {
-				loadTemplate($('#content div'), '#template-' + pagename, 'general.html', data, callback)
+				loadTemplate($('#content div'), '#template-' + pagename, 'general.html', data, function(){})
 			});
 		});
 	}
-}
-
-function callback() {
-	$('#loading').hide();
-	$('#content').slideDown('1500');
+	*/
 }
 
 /*
