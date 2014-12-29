2c924bbf89ebf9e01e8eb7e78740e01fa880594d
ybot1122 <ybot1122@gmail.com>
2014-12-28 19:18:17 -0800

    admin panel can now add new rows and submits properly

--- js/admin.js
+++ js/admin.js
@@ -64,10 +64,11 @@ function buildFrame(content) {
 	fields should be an array of strings, specifying which fields exist
 */
 function buildFormTable(data) {
-	$table = $('<table></table>');
-	$firstRow = $('<tr></tr>');
+	var keys = Object.keys(data.raw[0].M).sort();
+	var $table = $('<table></table>');
+	var $firstRow = $('<tr></tr>');
 	for (var i in data.fields) {
-		$th = $('<th></th>');
+		var $th = $('<th></th>');
 		$th.html(data.fields[i]);
 		$firstRow.append($th);
 	}
@@ -76,11 +77,10 @@ function buildFormTable(data) {
 	// populate table with content
 	for (var item in data.raw) {
 		var currData = data.raw[item].M;
-		var keys = Object.keys(currData).sort();
-		$row = $('<tr></tr>');
+		var $row = $('<tr></tr>');
 		for (var i in keys) {
-			$col = $('<td></td>');
-			$input = $('<input class="item" data-type="' 
+			var $col = $('<td></td>');
+			var $input = $('<input class="item" data-type="' 
 				+ keys[i] + '" type="text" />');
 			$input.attr('value', currData[keys[i]].S);
 			$col.html($input);
@@ -89,7 +89,39 @@ function buildFormTable(data) {
 		$row.append($('<td><input class="del" type="checkbox" /></td>'));
 		$table.append($row);
 	}
+	$table.append(createAddRowButton(keys));
 	return $table;
+}
+
+/*
+	Returns a <tr> element which contains a button that introduces
+	a new row of the page content
+*/
+function createAddRowButton(fields) {
+	var $row = $('<tr><td></td></tr>');
+	var $button = $('<span>Click to add new row</span>');
+	$button.click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		var $newRow = $('<tr></tr>');
+		var	$removeLink = $('<span>Remove this row</span>');
+		for (var i in fields) {
+			var $col = $('<td></td>');
+			var $input = $('<input class="item" data-type="' 
+				+ fields[i] + '" type="text" />');
+			$col.html($input);
+			$newRow.append($col);
+		}
+		$newRow.append($('<td></td>').append($removeLink));
+		$removeLink.one('click', function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			$(this).parents('tr').remove();
+		});
+		$row.before($newRow);
+	});
+	$row.html($button);
+	return $row; 
 }
 
 // destroys the admin modal and restores the main website
@@ -129,33 +161,10 @@ function adminNavButtonBehavior(ind, element) {
 		$('#' + formName + '-form').show();
 		$('#buttonHolder').show();
 	});
-	$('#' + formName + '-form #addRow').click(function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		var fields = $(this).data('fields').split(" ");
-		var $row = $('<tr></tr>');
-		var	$removeLink = $('<a></a>', {href: '#'});
-		$removeLink.one('click', function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-			$(this).parents('tr').remove();
-		})
-		$removeLink.html('Remove Row');
-		for (var i in fields) {
-			if (fields[i] === 'body') {
-				$row.append($('<td></td>').html('<textarea data-type="body"></textarea>'));
-			} else {
-				$row.append($('<td></td>').html('<input class="item" data-type="' + fields[i] + '" type="text" />'));
-			}
-		}
-		$row.append($('<td></td>').append($removeLink));
-
-		$('#' + formName + '-form').find('#addRow').parents('tr').before($row);
-	});
 }
 
 // invoked when user submits changes from the admin panel
-function deliverUpdateObject() {
+function deliverUpdateObject(resetApp) {
 	var result = {};
 	$('#buttons').find('span').not('#exit').each(function(ind, element) {
 		var pagename = $(element).attr('id');
@@ -177,13 +186,5 @@ function deliverUpdateObject() {
 	var resultString = JSON.stringify(result);
 	authenticateFb('update&upData=' + resultString, function(response) {
 		destroyModal(resetApp);
-	});
-}
-
-function loadTemplate($destination, selector, filename, data, callback) {
-	$destination.load('templates/' + filename + ' ' + selector,
-	function(response, status, xhr) {
-		$destination.html(Mustache.render($destination.text(), data));
-		callback();
 	});
 }
